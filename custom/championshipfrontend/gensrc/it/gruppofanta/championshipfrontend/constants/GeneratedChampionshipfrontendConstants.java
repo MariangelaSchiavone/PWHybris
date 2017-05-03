cd5df31f7ad93d61fc85b071a9721161eca15218
Carmine Di Gruttola <c.digruttola@hotmail.it>
2017-05-04 00:02:57 +0200

    championship-items created

---
+++ custom/championshipfrontend/gensrc/it/gruppofanta/championshipfrontend/constants/GeneratedChampionshipfrontendConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 3-mag-2017 23.21.30                         ---
+ * ----------------------------------------------------------------
+ */
+package it.gruppofanta.championshipfrontend.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedChampionshipfrontendConstants
+{
+	public static final String EXTENSIONNAME = "championshipfrontend";
+	
+	protected GeneratedChampionshipfrontendConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ custom/championshiprace/gensrc/it/gruppofanta/championshiprace/constants/GeneratedChampionshipraceConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 3-mag-2017 23.21.30                         ---
+ * ----------------------------------------------------------------
+ */
+package it.gruppofanta.championshiprace.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedChampionshipraceConstants
+{
+	public static final String EXTENSIONNAME = "championshiprace";
+	
+	protected GeneratedChampionshipraceConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
